54cab823a28657324506158e4df0b1a02291bb85
jonadableite <jonadab.leite@gmail.com>
2024-10-31 12:58:10 -0300

    fix: tratar valores de entrada inválidos no campo de conversão de moeda
    - Adicionada verificação para evitar resultados NaN quando o campo de valor está vazio ou contém um valor inválido.
    - Exibida mensagem "Insira um valor válido" caso o valor seja zero ou não numérico, garantindo uma experiência de usuário mais clara.
    - Ajuste na lógica de conversão para definir valor padrão (0) quando o campo está vazio.

--- scripts.js
+++ scripts.js
@@ -2,7 +2,8 @@ const convertButton = document.querySelector('.convert-button');
 const currencySelect = document.querySelector('.currency-select');
 
 function convertValue() {
-  const inputCurrencyValue = parseFloat(document.querySelector('.input-valor').value);
+  const inputCurrencyField = document.querySelector('.input-valor');
+  const inputCurrencyValue = parseFloat(inputCurrencyField.value) || 0; // Define como 0 se for NaN
   const currencyValueToConvert = document.querySelector('.real-value'); // valor em real
   const currencyValueConverted = document.querySelector('.dolar-value'); // valor a converter
 
@@ -11,6 +12,13 @@ function convertValue() {
   const bitcoinToday = 417.34;
 
   let convertedValue;
+
+  // Verifica se o campo de entrada está vazio ou com valor inválido
+  if (isNaN(inputCurrencyValue) || inputCurrencyValue <= 0) {
+    currencyValueConverted.innerHTML = 'Insira um valor válido';
+    currencyValueToConvert.innerHTML = '';
+    return;
+  }
 
   // Defina o valor convertido com base na moeda selecionada
   if (currencySelect.value === 'dolar') {
@@ -31,10 +39,7 @@ function convertValue() {
 
   if (currencySelect.value === 'bitcoin') {
     convertedValue = inputCurrencyValue / bitcoinToday;
-    currencyValueConverted.innerHTML = new Intl.NumberFormat('en-US', {
-      style: 'currency',
-      currency: 'BTC',
-    }).format(convertedValue);
+    currencyValueConverted.innerHTML = `₿ ${convertedValue.toFixed(6)}`; // Usando o símbolo manualmente
   }
 
   // Atualiza o valor em reais (BRL) na interface
@@ -44,4 +49,27 @@ function convertValue() {
   }).format(inputCurrencyValue);
 }
 
+function changeCurrency() {
+  const currencyName = document.querySelector('.dolar');
+  const currencyImage = document.querySelector('.dolar-img');
+
+  if (currencySelect.value === 'dolar') {
+    currencyName.innerHTML = 'US$ Dólar Americano';
+    currencyImage.src = './assets/dolar.png';
+  }
+
+  if (currencySelect.value === 'euro') {
+    currencyName.innerHTML = '€ Euro';
+    currencyImage.src = './assets/euro.png';
+  }
+
+  if (currencySelect.value === 'bitcoin') {
+    currencyName.innerHTML = '₿ Bitcoin';
+    currencyImage.src = './assets/bitcoin.png';
+  }
+
+  convertValue();
+}
+
+currencySelect.addEventListener('change', changeCurrency);
 convertButton.addEventListener('click', convertValue);
